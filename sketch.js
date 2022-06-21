2fa802163ecd781c499688049235cc0c8a94d6d2
sarayu :) <104691852+SarayuN16@users.noreply.github.com>
2022-06-21 12:22:17 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var box1;
+function setup() 
+{
+  createCanvas(400, 400);
+  box1 = new Box(250,300,20,20,2);
+}
+
+function draw() 
+{
+  background(220);
+  box1.show();
+  box1.move();
+
+  //drawSprites();
+}
